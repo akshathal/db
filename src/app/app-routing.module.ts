d40f0891d9bebd32946a4a121df7354e511690d1
Kavanab <kavanabhaskar@gmail.com>
2021-10-03 23:29:35 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,22 +2,17 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { ConceptValueComponent } from './concept-value/concept-value.component';
-import { ConceptComponent } from './concept-value/concept/concept.component';
-import { ValueComponent } from './concept-value/value/value.component';
 import { GraphviewComponent } from './knowledge/graphview/graphview.component';
 import { KnowledgeComponent } from './knowledge/knowledge.component';
-import { MetaknowledgeComponent } from './meta-knowledge/metaknowledge.component';
 import { RawviewComponent } from './knowledge/rawview/rawview.component';
-import { TraindataComponent } from './meta-knowledge/traindata/traindata.component';
 
 const routes: Routes = [
  // {   path: '', component: AppComponent },
    { path: '', component:KnowledgeComponent},
   // { path:'topicloud', component:MetaknowledgeComponent},
-   { path:'rawview',component:RawviewComponent},
-   { path:'graphview', component:GraphviewComponent},
-   { path: 'traindata', component:TraindataComponent}
+  //  { path:'rawview',component:RawviewComponent},
+  //  { path:'graphview', component:GraphviewComponent},
+  //  { path: 'traindata', component:TraindataComponent}
          /* children: [
             { path:'rawview',component:ConceptComponent},
             { path: 'graphview', component:ConceptValueComponent}
